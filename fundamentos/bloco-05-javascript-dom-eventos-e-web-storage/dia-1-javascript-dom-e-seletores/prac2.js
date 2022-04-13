06dc8519297e36fc9439dc871e1ad913e1e9e327
Lucas <lucasfernandomacedo13@gmail.com>
2022-04-13 17:45:35 -0300

    Resolução dos exercícios para fixar do dia 5.1

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/prac2.js
@@ -0,0 +1,5 @@
+document.querySelector("#header-container").style.backgroundColor = "#2fc18c";
+
+for (let i = 0; i < document.querySelectorAll(".emergency-tasks h3").length; i += 1) {
+  document.querySelectorAll(".emergency-tasks h3")[i].style.backgroundColor = "#9400D3"
+};
