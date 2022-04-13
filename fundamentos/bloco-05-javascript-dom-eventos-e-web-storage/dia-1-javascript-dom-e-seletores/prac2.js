de0ce252b73475f7bf88440ff0c41cdda5b505ce
Lucas <lucasfernandomacedo13@gmail.com>
2022-04-13 19:24:37 -0300

    Exercícios do dia 5.1

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/prac2.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/prac2.js
@@ -1,5 +1,15 @@
-document.querySelector("#header-container").style.backgroundColor = "#2fc18c";
+document.querySelector("#header-container").style.backgroundColor = "rgb(76,164,109)";
 
 for (let i = 0; i < document.querySelectorAll(".emergency-tasks h3").length; i += 1) {
   document.querySelectorAll(".emergency-tasks h3")[i].style.backgroundColor = "#9400D3"
 };
+
+for (let i = 0; i < document.querySelectorAll(".no-emergency-tasks h3").length; i += 1) {
+  document.querySelectorAll(".no-emergency-tasks h3")[i].style.backgroundColor = "black"
+};
+
+document.querySelector('.emergency-tasks').style.backgroundColor = "#E5906A";
+
+document.querySelector('.no-emergency-tasks').style.backgroundColor = "#FFDB58";
+
+document.querySelector('#footer-container').style.backgroundColor = "#1D3825";
