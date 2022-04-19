600c011ad432b66241e4debf51be8ae63a1f9098
Lucas <lucasfernandomacedo13@gmail.com>
2022-04-19 12:48:39 -0300

    Resolução dos exercícios do 3 ao 9 do dia 5.3

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercises/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercises/script.js
@@ -17,6 +17,7 @@ createDaysOfTheWeek();
 
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
+// Exercício 1
 let days = document.querySelector('#days');
 for (i = 0; i < dezDaysList.length; i += 1) {
   let li = document.createElement('li');
@@ -34,7 +35,8 @@ lis[12].classList.add('friday');
 lis[19].classList.add('friday');
 lis[26].classList.add('friday');
 
-function buttonMaker(buttonName) {
+// Exercício 2
+function buttonMakerHoliday(buttonName) {
   let button = document.createElement('button');
   button.setAttribute('id', 'btn-holiday');
   button.innerText = buttonName
@@ -42,5 +44,82 @@ function buttonMaker(buttonName) {
   buttonContainer.appendChild(button);
 }
 
-buttonMaker('Feriados');
+buttonMakerHoliday('Feriados');
 
+// Exercício 3 (não acabado, nao consigo voltar pra branco e está dando um erro no console)
+let button = document.querySelector('#btn-holiday');
+button.addEventListener("click", function () {
+  let holiday = document.querySelectorAll('.holiday');
+  for (let item in holiday) {
+    holiday[item].style.backgroundColor = 'lightGreen'
+  }
+});
+
+// Exercício 4
+function buttonMakerFriday(buttonName) {
+  let button = document.createElement('button');
+  button.setAttribute('id', 'btn-friday');
+  button.innerText = buttonName
+  let buttonContainer = document.querySelector('.buttons-container');
+  buttonContainer.appendChild(button);
+}
+
+buttonMakerFriday('Sexta-feira');
+
+// Exercício 5 (Não acabado, mesma coisa do 3, não sei voltar)
+let buttonFriday = document.querySelector('#btn-friday');
+buttonFriday.addEventListener("click", function () {
+  let friday = document.querySelectorAll('.friday');
+  for (let item in friday) {
+    friday[item].innerText = 'SEXTOU!'
+  }
+});
+
+// Exercício 6
+let listElements = document.querySelectorAll('.day');
+
+for (let i = 0; i < listElements.length; i += 1) {
+  listElements[i].addEventListener('mouseover', function(event) {
+    let day = event.target
+    day.style.fontSize = 'xx-large'
+  });
+  listElements[i].addEventListener('mouseout', function(event) {
+    let day = event.target
+    day.style.fontSize = 'medium'
+  });
+};
+
+// Exercício 7
+function addTask(task) {
+  let tasks = document.createElement('span');
+  tasks.innerText = task + '\n'
+  let taskContainer = document.querySelector('.my-tasks');
+  taskContainer.appendChild(tasks);
+}
+
+addTask("Cozinhar");
+addTask("Ir a academia");
+addTask("Caminhar");
+
+// Exercício 8
+function addColor(color) {
+  let div = document.createElement('div');
+  div.setAttribute('class', 'task');
+  div.style.backgroundColor = color;
+  let taskContainer = document.querySelector('.my-tasks');
+  taskContainer.appendChild(div);
+}
+
+addColor('red');
+addColor('green');
+addColor('blue');
+
+// Exercício 9 (Não acabado, mesma coisa do 3, não sei voltar)
+let listOfTasks = document.querySelectorAll('.task')
+for (i = 0; i < listOfTasks.length; i += 1) {
+  listOfTasks[i].addEventListener('click', function(event) {
+    event.target.classList.add('selected')
+  })
+}
+
+// Exercício 10
